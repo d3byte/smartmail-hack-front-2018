5958bc066df1c84d2f206b8cfd322d14416ec519
Sergey Savtyra <pandinator@protonmail.com>
2018-07-14 11:26:51 +0300

    Подключил зависимости VueMaterial и VueResource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(VueResource)
+Vue.use(VueMaterial)
+
+Vue.http.options.root = ''
 
 Vue.config.productionTip = false
 
