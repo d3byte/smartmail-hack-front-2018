ebb58b14b0d6b5f2f6d70bac991e9154b8d7c655
LaExploradora <laexploradora@msk-wifi-1fap13-laexplodorasair-noname.mail.msk>
2018-07-14 19:16:18 +0300

    Поменял цвет бэкграунда на чистый белый.

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -67,125 +67,118 @@
 
 <script>
 export default {
-    props: ['collapsed'],
-    data() {
-        return {
-            toggled: [],
-            active: null,
-        }
+  props: ["collapsed"],
+  data() {
+    return {
+      toggled: [],
+      active: null
+    };
+  },
+  methods: {
+    toggle(id) {
+      if (this.toggled.includes(id)) {
+        this.toggled = this.toggled.filter(item => item !== id);
+        return;
+      }
+      this.toggled.push(id);
     },
-    methods: {
-        toggle(id) {
-            if (this.toggled.includes(id)) {
-                this.toggled = this.toggled.filter(item => item !== id)
-                return
-            }
-            this.toggled.push(id)
-        },
-        select(id) {
-            this.active = id
-        },
-        collapse() {
-            this.$emit('input', !this.collapsed)
-        }
+    select(id) {
+      this.active = id;
+    },
+    collapse() {
+      this.$emit("input", !this.collapsed);
     }
-}
+  }
+};
 </script>
 
 <style lang="scss">
 .side-menu {
-    width: 20%;
-    height: 100vh;
-    border-right: 1px solid #b9b9b9;
-    background: rgb(240, 240, 240);
+  width: 20%;
+  height: 100vh;
+  border-right: 1px solid #b9b9b9;
+  background: rgb(255, 255, 255);
 
-    &.collapsed {
-        width: 70px;
+  &.collapsed {
+    width: 70px;
 
-        ul li {
-            .menu-item {
-                    & > input[type="checkbox"], & > span, & > img {
-                        display: none;
-                    }
-                }
+    ul li {
+      .menu-item {
+        & > input[type="checkbox"],
+        & > span,
+        & > img {
+          display: none;
         }
-    } 
-
-    ul {
-        margin: 0;
-        padding: 0 15px;
-        list-style: none;
-
-        li {
-
-            &.active {
-                .image-wrapper {
-                    background: rgb(221, 221, 221);
-                }
-            }
-
-            .menu-item {
-                display: flex;
-                align-items: center;
-                position: relative;
-                cursor: pointer;
-
-                &.active {
-                    background: rgb(221,221, 221);
-                    border-radius: 8px;
-                    font-weight: bold;
-                }
-
-                .image-wrapper {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    width: 40px;
-                    height: 40px;
-                    padding: 10px;
-                    border-radius: 4px;
-
-                    // Icon
-                    img {
-                        width: 17px;
-                    }
-                }
-
-                & > img {
-                    width: 7px;
-                    // margin-left: 10px;
-                    transform: rotate(180deg);
-                }
-
-                & > input[type="checkbox"] {
-                    opacity: 0;
-                    position: absolute;
-                    width: 100%;
-                    height: 100%;
-                    cursor: pointer;
-
-                    &:checked {
-                        & ~ img {
-                            transform: rotate(0);
-                        }
-
-
-
-                    }
-
-                }
-
-            }
-
-            .sub-menu {
-                &:not(.toggled) {
-                    display: none;
-                }
-
-            }
-
-        }
-
+      }
     }
+  }
+
+  ul {
+    margin: 0;
+    padding: 0 15px;
+    list-style: none;
+
+    li {
+      &.active {
+        .image-wrapper {
+          background: rgb(221, 221, 221);
+        }
+      }
+
+      .menu-item {
+        display: flex;
+        align-items: center;
+        position: relative;
+        cursor: pointer;
+
+        &.active {
+          background: rgb(221, 221, 221);
+          border-radius: 8px;
+          font-weight: bold;
+        }
+
+        .image-wrapper {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 40px;
+          height: 40px;
+          padding: 10px;
+          border-radius: 4px;
+
+          // Icon
+          img {
+            width: 17px;
+          }
+        }
+
+        & > img {
+          width: 7px;
+          // margin-left: 10px;
+          transform: rotate(180deg);
+        }
+
+        & > input[type="checkbox"] {
+          opacity: 0;
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+
+          &:checked {
+            & ~ img {
+              transform: rotate(0);
+            }
+          }
+        }
+      }
+
+      .sub-menu {
+        &:not(.toggled) {
+          display: none;
+        }
+      }
+    }
+  }
 }
 </style>
