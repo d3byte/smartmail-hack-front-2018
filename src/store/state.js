a220df9fbf60b292dcd3554b607db91ef38ed08a
Pavel <pablo.osamu@yandex,ru>
2018-07-14 17:01:17 +0300

    Files and icons

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,103 @@
+import gp from './assets/docx/3gp.png'
+import z from './assets/docx/7z.png'
+import ae from './assets/docx/ae.png'
+import ai from './assets/docx/ai.png'
+import apk from './assets/docx/apk.png'
+import asf from './assets/docx/asf.png'
+import avi from './assets/docx/avi.png'
+import bak from './assets/docx/bak.png'
+import bmp from './assets/docx/bmp.png'
+import cdr from './assets/docx/cdr.png'
+import css from './assets/docx/css.png'
+import csv from './assets/docx/csv.png'
+import divx from './assets/docx/divx.png'
+import dll from './assets/docx/dll.png'
+import doc from './assets/docx/doc.png'
+import docx from './assets/docx/docx.png'
+import dw from './assets/docx/dw.png'
+import dwg from './assets/docx/dwg.png'
+import eps from './assets/docx/eps.png'
+import exe from './assets/docx/exe.png'
+import flv from './assets/docx/flv.png'
+import fw from './assets/docx/fw.png'
+import gif from './assets/docx/gif.png'
+import gz from './assets/docx/gz.png'
+import html from './assets/docx/html.png'
+import ico from './assets/docx/ico.png'
+import iso from './assets/docx/iso.png'
+import jar from './assets/docx/jar.png'
+import jpg from './assets/docx/jpg.png'
+import js from './assets/docx/js.png'
+import mov from './assets/docx/mov.png'
+import mp3 from './assets/docx/mp3.png'
+import mp4 from './assets/docx/mp4.png'
+import mpeg from './assets/docx/mpeg.png'
+import pdf from './assets/docx/pdf.png'
+import php from './assets/docx/php.png'
+import png from './assets/docx/png.png'
+import ppt from './assets/docx/ppt.png'
+import ps from './assets/docx/ps.png'
+import psd from './assets/docx/psd.png'
+import rar from './assets/docx/rar.png'
+import svg from './assets/docx/svg.png'
+import swf from './assets/docx/swf.png'
+import sys from './assets/docx/sys.png'
+import tar from './assets/docx/tar.png'
+import tiff from './assets/docx/tiff.png'
+import txt from './assets/docx/txt.png'
+import wav from './assets/docx/wav.png'
+import zip from './assets/docx/zip.png'
+
 export default {
-    
+    icons: [
+        {name: '3gp.png', data: gp},
+        {name: '7z.png', data: z},
+        {name: 'ae.png', data: ae},
+        {name: 'ai.png', data: ai},
+        {name: 'apk.png', data: apk},
+        {name: 'asf.png', data: asf},
+        {name: 'avi.png', data: avi},
+        {name: 'bak.png', data: bak},
+        {name: 'bmp.png', data: bmp},
+        {name: 'cdr.png', data: cdr},
+        {name: 'css.png', data: css},
+        {name: 'csv.png', data: csv},
+        {name: 'divx.png', data: divx},
+        {name: 'dll.png', data: dll},
+        {name: 'doc.png', data: doc},
+        {name: 'docx.png', data: docx},
+        {name: 'dw.png', data: dw},
+        {name: 'dwg.png', data: dwg},
+        {name: 'eps.png', data: eps},
+        {name: 'exe.png', data: exe},
+        {name: 'flv.png', data: flv},
+        {name: 'fx.png', data: fx},
+        {name: 'gif.png', data: gif},
+        {name: 'gz.png', data: gz},
+        {name: 'html.png', data: html},
+        {name: 'ico.png', data: ico},
+        {name: 'iso.png', data: iso},
+        {name: 'jar.png', data: jar},
+        {name: 'jpg.png', data: jpg},
+        {name: 'js.png', data: js},
+        {name: 'mov.png', data: mov},
+        {name: 'mp3.png', data: mp3},
+        {name: 'mp4.png', data: mp4},
+        {name: 'mpeg.png', data: mpeg},
+        {name: 'pdf.png', data: pdf},
+        {name: 'php.png', data: php},
+        {name: 'png.png', data: png},
+        {name: 'ppt.png', data: ppt},
+        {name: 'ps.png', data: ps},
+        {name: 'psd.png', data: psd},
+        {name:'rar.png', data: rar},
+        {name: 'svg.png', data: svg},
+        {name: 'swf.png', data: swf},
+        {name: 'sys.png', data: sys},
+        {name: 'tar.png', data: tar},
+        {name: 'tiff.png', data: tiff},
+        {name: 'txt.png', data: txt},
+        {name: 'wav.png', data: wav},
+        {name: 'zip.png', data: zip},
+        ],
 }
