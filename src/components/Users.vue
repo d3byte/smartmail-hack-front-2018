7f70a3f00ee5acc1a0f55f16aa5268d45fdd0692
LaExploradora <laexploradora@msk-wifi-1fap13-laexplodorasair-noname.mail.msk>
2018-07-14 17:12:45 +0300

    Добавил границу справа

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -17,190 +17,194 @@
 
 <script>
 export default {
-    data() {
-        return {
-            idActiveUser: null,
-            backButton: null,
-            users: [
-                {
-                    id: 0,
-                    name: "Vasya Pupkin",
-                    subject: "Subject 1",
-                    isActive: false
-                },
-                {
-                    id: 1,
-                    name: "Vasya Sisya",
-                    subject: "Subject 2",
-                    isActive: false
-                },
-                {
-                    id: 2,
-                    name: "Vasya Pisya",
-                    subject: "Subject 3",
-                    isActive: false
-                },
-                {
-                    id: 3,
-                    name: "Vasya Sisya",
-                    subject: "Subject 2",
-                    isActive: false
-                },
-                {
-                    id: 4,
-                    name: "Vasya Pisya",
-                    subject: "Subject 3",
-                    isActive: false
-                },
-                {
-                    id: 5,
-                    name: "Vasya Sisya",
-                    subject: "Subject 2",
-                    isActive: false
-                },
-                {
-                    id: 6,
-                    name: "Vasya Pisya",
-                    subject: "Subject 3",
-                    isActive: false
-                },
-                {
-                    id: 7,
-                    name: "Vasya Sisya",
-                    subject: "Subject 2",
-                    isActive: false
-                },
-                {
-                    id: 8,
-                    name: "Vasya Pisya",
-                    subject: "Subject 3",
-                    isActive: false
-                },
-            ]
-        }
-    },
-    methods: {
-        curl(id) {
-            this.idActiveUser = id
-            this.backButton = id
-            return
+  name: "Users",
+  data() {
+    return {
+      idActiveUser: null,
+      backButton: null,
+      users: [
+        {
+          id: 0,
+          name: "Vasya Pupkin",
+          subject: "Email",
+          isActive: false
         },
-        backButtonEvent() {
-            this.backButton = null
-            return
+        {
+          id: 1,
+          name: "Vasya Sisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 2,
+          name: "Vasya Pisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 3,
+          name: "Vasya Sisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 4,
+          name: "Vasya Pisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 5,
+          name: "Vasya Sisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 6,
+          name: "Vasya Pisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 7,
+          name: "Vasya Sisya",
+          subject: "Email",
+          isActive: false
+        },
+        {
+          id: 8,
+          name: "Vasya Pisya",
+          subject: "Email",
+          isActive: false
         }
+      ]
+    };
+  },
+  methods: {
+    curl(id) {
+      this.idActiveUser = id;
+      this.backButton = id;
+      return;
+    },
+    backButtonEvent() {
+      this.backButton = null;
+      return;
     }
-}
+  }
+};
 </script>
 
 <style lang="scss">
 section.users {
+  margin: 0;
+  padding: 0px;
+  background: #cecece;
+  width: 320px;
+  height: 100vh;
+  ms-user-select: none;
+  -moz-user-select: none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+  overflow: scroll;
+  border-right: 1px solid #b9b9b9;
+
+  .users-item {
+    border-bottom: 1px solid #b9b9b9;
     margin: 0;
-    padding: 0px;
-    background: #CECECE;
-    width: 320px;
-    height: 100vh;
-    ms-user-select: none;
-    -moz-user-select: none;
-    -khtml-user-select: none;
-    -webkit-user-select: none;
-    overflow: scroll;
+    padding: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    cursor: pointer;
 
-    .users-item {
-        border-bottom: 1px solid #b9b9b9;
+    &:hover,
+    &:active {
+      background: #c1c1c1;
+    }
+
+    &.active {
+      background: #b9b9b9;
+    }
+
+    .user-img {
+      padding: 20px;
+      margin: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      img {
+        width: 48px;
+        height: 48px;
+        border: 1px solid rgba(0, 0, 0, 0.12);
+        border-radius: 50%;
+      }
+    }
+
+    .user-data {
+      margin: 0;
+      padding: 0;
+      line-height: 1.7;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      h4,
+      p {
         margin: 0;
-        padding: 5px;
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        cursor: pointer;
+        font-family: Arial, Helvetica, sans-serif;
+        color: #333333;
+      }
 
-        &:hover, &:active {
-            background: #c1c1c1;
-        }
+      h4 {
+        font-size: 15px;
+        font-weight: normal;
+      }
 
-        &.active {
-            background: #b9b9b9;
-        }
+      p {
+        font-size: 13px;
+      }
+    }
+  }
 
-        .user-img {
-            padding: 20px;
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
+  .back {
+    display: none;
+    -webkit-transition: top 1s ease-out 0.5s;
+    -moz-transition: top 1s ease-out 0.5s;
+    -o-transition: top 1s ease-out 0.5s;
+    transition: top 1s ease-out 0.5s;
+  }
 
-            img {
-                width: 48px;
-                height: 48px;
-                border: 1px solid rgba(0,0,0,0.12);
-                border-radius: 50%;
-            }
-        }
+  &.hidden {
+    width: 100px;
+    padding-top: 40px;
 
-        .user-data {
-            margin: 0;
-            padding: 0;
-            line-height: 1.7;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-
-            h4, p {
-                margin: 0;
-                font-family: Arial, Helvetica, sans-serif;
-                color: #333333;
-            }
-
-            h4 {
-                font-size: 15px;
-                font-weight: normal;
-            }
-
-            p {
-                font-size: 13px;
-            }
-        }
+    .user-data {
+      display: none !important;
     }
 
     .back {
-        display: none;
-        -webkit-transition: top 1s ease-out 0.5s;
-        -moz-transition: top 1s ease-out 0.5s;
-        -o-transition: top 1s ease-out 0.5s;
-        transition: top 1s ease-out 0.5s;
+      display: flex;
+      height: 40px;
+      width: 100px;
+      position: fixed;
+      top: 50px !important;
+      background: #cecece;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      -webkit-transition: top 1s ease-out 0.5s;
+      -moz-transition: top 1s ease-out 0.5s;
+      -o-transition: top 1s ease-out 0.5s;
+      transition: top 1s ease-out 0.5s;
+      border-bottom: 1px solid #b9b9b9;
+
+      img {
+        width: 10px;
+        height: 10px;
+      }
     }
-    
-    &.hidden {
-        width: 100px;
-        padding-top: 40px;
-
-        .user-data {
-            display: none !important;
-        }
-
-        .back {
-            display: flex;
-            height: 40px;
-            width: 100px;
-            position: fixed;
-            top: 0;
-            background: #CECECE;
-            justify-content: center;
-            align-items: center;
-            cursor: pointer;
-            -webkit-transition: top 1s ease-out 0.5s;
-            -moz-transition: top 1s ease-out 0.5s;
-            -o-transition: top 1s ease-out 0.5s;
-            transition: top 1s ease-out 0.5s;
-            border-bottom: 1px solid #b9b9b9;
-
-            img {
-                width: 10px;
-                height: 10px;
-            }
-        }
-    }
+  }
 }
 </style>
 
