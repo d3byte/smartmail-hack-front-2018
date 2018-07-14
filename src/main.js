3bd588c47bd24d10287f54c34893c7f20fc88468
Sergey Savtyra <pandinator@protonmail.com>
2018-07-14 12:14:38 +0300

    Кек

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.use(VueResource)
-Vue.use(VueMaterial)
 
 Vue.http.options.root = ''
 
