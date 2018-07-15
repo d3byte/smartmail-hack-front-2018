7d09b7ee27079dcec76b9a648ca09594ef91ba68
LaExploradora <laexploradora@msk-wifi-1fap11-laexplodorasair-noname.mail.msk>
2018-07-15 15:00:32 +0300

    Добавил жпЕг.

--- src/store/state.js
+++ src/store/state.js
@@ -1,124 +1,125 @@
-import gp from '../assets/docx/3gp.png'
-import z from '../assets/docx/7z.png'
-import ae from '../assets/docx/ae.png'
-import ai from '../assets/docx/ai.png'
-import apk from '../assets/docx/apk.png'
-import asf from '../assets/docx/asf.png'
-import avi from '../assets/docx/avi.png'
-import bak from '../assets/docx/bak.png'
-import bmp from '../assets/docx/bmp.png'
-import cdr from '../assets/docx/cdr.png'
-import css from '../assets/docx/css.png'
-import csv from '../assets/docx/csv.png'
-import divx from '../assets/docx/divx.png'
-import dll from '../assets/docx/dll.png'
-import doc from '../assets/docx/doc.png'
-import docx from '../assets/docx/docx.png'
-import dw from '../assets/docx/dw.png'
-import dwg from '../assets/docx/dwg.png'
-import eps from '../assets/docx/eps.png'
-import exe from '../assets/docx/exe.png'
-import file from '../assets/docx/file.png'
-import flv from '../assets/docx/flv.png'
-import fw from '../assets/docx/fw.png'
-import gif from '../assets/docx/gif.png'
-import gz from '../assets/docx/gz.png'
-import html from '../assets/docx/html.png'
-import ico from '../assets/docx/ico.png'
-import iso from '../assets/docx/iso.png'
-import jar from '../assets/docx/jar.png'
-import jpg from '../assets/docx/jpg.png'
-import js from '../assets/docx/js.png'
-import mov from '../assets/docx/mov.png'
-import mp3 from '../assets/docx/mp3.png'
-import mp4 from '../assets/docx/mp4.png'
-import mpeg from '../assets/docx/mpeg.png'
-import pdf from '../assets/docx/pdf.png'
-import php from '../assets/docx/php.png'
-import png from '../assets/docx/png.png'
-import ppt from '../assets/docx/ppt.png'
-import ps from '../assets/docx/ps.png'
-import psd from '../assets/docx/psd.png'
-import rar from '../assets/docx/rar.png'
-import svg from '../assets/docx/svg.png'
-import swf from '../assets/docx/swf.png'
-import sys from '../assets/docx/sys.png'
-import tar from '../assets/docx/tar.png'
-import tiff from '../assets/docx/tiff.png'
-import txt from '../assets/docx/txt.png'
-import wav from '../assets/docx/wav.png'
-import zip from '../assets/docx/zip.png'
+import gp from "../assets/docx/3gp.png";
+import z from "../assets/docx/7z.png";
+import ae from "../assets/docx/ae.png";
+import ai from "../assets/docx/ai.png";
+import apk from "../assets/docx/apk.png";
+import asf from "../assets/docx/asf.png";
+import avi from "../assets/docx/avi.png";
+import bak from "../assets/docx/bak.png";
+import bmp from "../assets/docx/bmp.png";
+import cdr from "../assets/docx/cdr.png";
+import css from "../assets/docx/css.png";
+import csv from "../assets/docx/csv.png";
+import divx from "../assets/docx/divx.png";
+import dll from "../assets/docx/dll.png";
+import doc from "../assets/docx/doc.png";
+import docx from "../assets/docx/docx.png";
+import dw from "../assets/docx/dw.png";
+import dwg from "../assets/docx/dwg.png";
+import eps from "../assets/docx/eps.png";
+import exe from "../assets/docx/exe.png";
+import file from "../assets/docx/file.png";
+import flv from "../assets/docx/flv.png";
+import fw from "../assets/docx/fw.png";
+import gif from "../assets/docx/gif.png";
+import gz from "../assets/docx/gz.png";
+import html from "../assets/docx/html.png";
+import ico from "../assets/docx/ico.png";
+import iso from "../assets/docx/iso.png";
+import jar from "../assets/docx/jar.png";
+import jpg from "../assets/docx/jpg.png";
+import js from "../assets/docx/js.png";
+import mov from "../assets/docx/mov.png";
+import mp3 from "../assets/docx/mp3.png";
+import mp4 from "../assets/docx/mp4.png";
+import mpeg from "../assets/docx/mpeg.png";
+import pdf from "../assets/docx/pdf.png";
+import php from "../assets/docx/php.png";
+import png from "../assets/docx/png.png";
+import ppt from "../assets/docx/ppt.png";
+import ps from "../assets/docx/ps.png";
+import psd from "../assets/docx/psd.png";
+import rar from "../assets/docx/rar.png";
+import svg from "../assets/docx/svg.png";
+import swf from "../assets/docx/swf.png";
+import sys from "../assets/docx/sys.png";
+import tar from "../assets/docx/tar.png";
+import tiff from "../assets/docx/tiff.png";
+import txt from "../assets/docx/txt.png";
+import wav from "../assets/docx/wav.png";
+import zip from "../assets/docx/zip.png";
 ///////////////////////////////////////
-import Excel from '../assets/thumbnails/excel.jpg'
-import Jpg from '../assets/thumbnails/jpg.jpg'
-import Pdf from '../assets/thumbnails/pdf.jpg'
-import Pdf1 from '../assets/thumbnails/pdf1.jpg'
-import Png from '../assets/thumbnails/png.jpg'
-import Png1 from '../assets/thumbnails/png1.jpg'
-import Ppt from '../assets/thumbnails/ppt.jpg'
-import Psd from '../assets/thumbnails/psd.jpg'
+import Excel from "../assets/thumbnails/excel.jpg";
+import Jpg from "../assets/thumbnails/jpg.jpg";
+import Pdf from "../assets/thumbnails/pdf.jpg";
+import Pdf1 from "../assets/thumbnails/pdf1.jpg";
+import Png from "../assets/thumbnails/png.jpg";
+import Png1 from "../assets/thumbnails/png1.jpg";
+import Ppt from "../assets/thumbnails/ppt.jpg";
+import Psd from "../assets/thumbnails/psd.jpg";
 
 export default {
-    icons: [
-        { name: '3gp', data: gp },
-        { name: '7z', data: z },
-        { name: 'ae', data: ae },
-        { name: 'ai', data: ai },
-        { name: 'apk', data: apk },
-        { name: 'asf', data: asf },
-        { name: 'avi', data: avi },
-        { name: 'bak', data: bak },
-        { name: 'bmp', data: bmp },
-        { name: 'cdr', data: cdr },
-        { name: 'css', data: css },
-        { name: 'csv', data: csv },
-        { name: 'divx', data: divx },
-        { name: 'dll', data: dll },
-        { name: 'doc', data: doc },
-        { name: 'docx', data: docx },
-        { name: 'dw', data: dw },
-        { name: 'dwg', data: dwg },
-        { name: 'eps', data: eps },
-        { name: 'exe', data: exe },
-        { name: 'file', data: file },
-        { name: 'flv', data: flv },
-        { name: 'fw', data: fw },
-        { name: 'gif', data: gif },
-        { name: 'gz', data: gz },
-        { name: 'html', data: html },
-        { name: 'ico', data: ico },
-        { name: 'iso', data: iso },
-        { name: 'jar', data: jar },
-        { name: 'jpg', data: jpg },
-        { name: 'js', data: js },
-        { name: 'mov', data: mov },
-        { name: 'mp3', data: mp3 },
-        { name: 'mp4', data: mp4 },
-        { name: 'mpeg', data: mpeg },
-        { name: 'pdf', data: pdf },
-        { name: 'php', data: php },
-        { name: 'png', data: png },
-        { name: 'ppt', data: ppt },
-        { name: 'ps', data: ps },
-        { name: 'psd', data: psd },
-        { name: 'rar', data: rar },
-        { name: 'svg', data: svg },
-        { name: 'swf', data: swf },
-        { name: 'sys', data: sys },
-        { name: 'tar', data: tar },
-        { name: 'tif', data: tiff },
-        { name: 'tiff', data: tiff },
-        { name: 'txt', data: txt },
-        { name: 'wav', data: wav },
-        { name: 'zip', data: zip },
-    ],
-    thumbnails: {
-        xls: [Excel],
-        jpg: [Jpg],
-        pdf: [Pdf, Pdf1],
-        png: [Png, Png1],
-        ppt: [Ppt],
-        pptx: [Ppt],
-        psd: [Psd]
-    },
-}
+  icons: [
+    { name: "3gp", data: gp },
+    { name: "7z", data: z },
+    { name: "ae", data: ae },
+    { name: "ai", data: ai },
+    { name: "apk", data: apk },
+    { name: "asf", data: asf },
+    { name: "avi", data: avi },
+    { name: "bak", data: bak },
+    { name: "bmp", data: bmp },
+    { name: "cdr", data: cdr },
+    { name: "css", data: css },
+    { name: "csv", data: csv },
+    { name: "divx", data: divx },
+    { name: "dll", data: dll },
+    { name: "doc", data: doc },
+    { name: "docx", data: docx },
+    { name: "dw", data: dw },
+    { name: "dwg", data: dwg },
+    { name: "eps", data: eps },
+    { name: "exe", data: exe },
+    { name: "file", data: file },
+    { name: "flv", data: flv },
+    { name: "fw", data: fw },
+    { name: "gif", data: gif },
+    { name: "gz", data: gz },
+    { name: "html", data: html },
+    { name: "ico", data: ico },
+    { name: "iso", data: iso },
+    { name: "jar", data: jar },
+    { name: "jpg", data: jpg },
+    { name: "jpeg", data: jpg },
+    { name: "js", data: js },
+    { name: "mov", data: mov },
+    { name: "mp3", data: mp3 },
+    { name: "mp4", data: mp4 },
+    { name: "mpeg", data: mpeg },
+    { name: "pdf", data: pdf },
+    { name: "php", data: php },
+    { name: "png", data: png },
+    { name: "ppt", data: ppt },
+    { name: "ps", data: ps },
+    { name: "psd", data: psd },
+    { name: "rar", data: rar },
+    { name: "svg", data: svg },
+    { name: "swf", data: swf },
+    { name: "sys", data: sys },
+    { name: "tar", data: tar },
+    { name: "tif", data: tiff },
+    { name: "tiff", data: tiff },
+    { name: "txt", data: txt },
+    { name: "wav", data: wav },
+    { name: "zip", data: zip }
+  ],
+  thumbnails: {
+    xls: [Excel],
+    jpg: [Jpg],
+    pdf: [Pdf, Pdf1],
+    png: [Png, Png1],
+    ppt: [Ppt],
+    pptx: [Ppt],
+    psd: [Psd]
+  }
+};
